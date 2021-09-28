fd64f0f463092c12246a4df9055453ce63bba7e6
Ethan Partidas <ethanpartidas@gmail.com>
2021-09-28 14:54:54 -0400

    refactor: improve method names

--- mnk.py
+++ mnk.py
@@ -9,6 +9,17 @@ class Board:
         self.board = [['_']*n for _ in range(m)]
         self.player = 'X'
         self.opponent = 'O'
+    # allows for printing of the current board state
+    def __str__(self):
+        string = ''
+        for row in reversed(list(zip(*self.board))):
+            for x, cell in enumerate(row):
+                string += cell
+                if x != self.m - 1:
+                    string += '|'
+                else:
+                    string += '\n'
+        return string
     def flip_players(self):
         self.player, self.opponent = self.opponent, self.player
     # does a move by changing the board and current player
@@ -19,27 +30,16 @@ class Board:
     def undo_move(self, x, y):
         self.board[x][y] = '_'
         self.flip_players()
-    # generates a list of all legal moves
-    def generate_moves(self):
+    # generates and returns a list of all legal moves
+    def legal_moves(self):
         moves = []
         for x, column in enumerate(self.board):
             for y, cell in enumerate(column):
                 if cell == '_':
                     moves.append((x, y))
         return moves
-    # allows for printing of the current board state
-    def __repr__(self):
-        string = ''
-        for row in reversed(list(zip(*self.board))):
-            for x, cell in enumerate(row):
-                string += cell
-                if x != self.m - 1:
-                    string += '|'
-                else:
-                    string += '\n'
-        return string
     # returns True if the player whose turn it is has lost, False otherwise
-    def lost(self):
+    def player_has_lost(self):
         # check vertical line |
         for column in self.board:
             count = 0
@@ -51,7 +51,7 @@ class Board:
                 if count == self.k:
                     return True
         # check horizontal line -
-        for row in list(zip(*self.board)):
+        for row in zip(*self.board):
             count = 0
             for cell in row:
                 if cell == self.opponent:
--- test_mnk.py
+++ test_mnk.py
@@ -4,39 +4,39 @@ import mnk
 class TestMnk(unittest.TestCase):
     def setUp(self):
         self.board = mnk.Board(3, 3, 2)
-    def test_repr(self):
-        self.assertEqual(repr(self.board), '_|_|_\n'*3)
+    def test_str(self):
+        self.assertEqual(str(self.board), '_|_|_\n'*3)
     def test_move(self):
         self.board.move(0, 0)
         self.board.move(1, 2)
         self.board.move(2, 1)
-        self.assertEqual(repr(self.board),
+        self.assertEqual(str(self.board),
             '_|O|_\n'
             '_|_|X\n'
             'X|_|_\n')
     def test_undo_move(self):
         self.board.move(0, 0)
         self.board.undo_move(0, 0)
-        self.assertEqual(repr(self.board), '_|_|_\n'*3)
-    def test_generate_moves(self):
-        self.assertEqual(len(self.board.generate_moves()), 9)
+        self.assertEqual(str(self.board), '_|_|_\n'*3)
+    def test_legal_moves(self):
+        self.assertEqual(len(self.board.legal_moves()), 9)
         self.board.move(0, 0)
-        self.assertEqual(len(self.board.generate_moves()), 8)
-    def test_lost(self):
+        self.assertEqual(len(self.board.legal_moves()), 8)
+    def test_player_has_lost(self):
         self.board.move(1, 1)
         self.board.move(2, 2)
-        self.assertFalse(self.board.lost())
+        self.assertFalse(self.board.player_has_lost())
         self.board.move(0, 0)
-        self.assertTrue(self.board.lost())
+        self.assertTrue(self.board.player_has_lost())
         self.board.undo_move(0, 0)
         self.board.move(1, 0)
-        self.assertTrue(self.board.lost())
+        self.assertTrue(self.board.player_has_lost())
         self.board.undo_move(1, 0)
         self.board.move(2, 0)
-        self.assertTrue(self.board.lost())
+        self.assertTrue(self.board.player_has_lost())
         self.board.undo_move(2, 0)
         self.board.move(2, 1)
-        self.assertTrue(self.board.lost())
+        self.assertTrue(self.board.player_has_lost())
 
 if __name__ == '__main__':
     unittest.main()
