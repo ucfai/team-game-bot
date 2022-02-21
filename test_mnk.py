8ff627cea15aaf9a23f91922106660e9993830bf
Ethan Partidas <ethanpartidas@gmail.com>
2022-02-20 23:17:41 -0500

    switch testing from unittest to pytest

--- test_mnk.py
+++ test_mnk.py
@@ -1,42 +1,44 @@
-import unittest
 import mnk
 
-class TestMnk(unittest.TestCase):
-    def setUp(self):
-        self.board = mnk.Board(3, 3, 2)
-    def test_str(self):
-        self.assertEqual(str(self.board), '_|_|_\n'*3)
-    def test_move(self):
-        self.board.move(0, 0)
-        self.board.move(1, 2)
-        self.board.move(2, 1)
-        self.assertEqual(str(self.board),
-            '_|O|_\n'
-            '_|_|X\n'
-            'X|_|_\n')
-    def test_undo_move(self):
-        self.board.move(0, 0)
-        self.board.undo_move()
-        self.assertEqual(str(self.board), '_|_|_\n'*3)
-    def test_legal_moves(self):
-        self.assertEqual(len(self.board.legal_moves()), 9)
-        self.board.move(0, 0)
-        self.assertEqual(len(self.board.legal_moves()), 8)
-    def test_player_has_lost(self):
-        self.board.move(1, 1)
-        self.board.move(2, 2)
-        self.assertFalse(self.board.player_has_lost())
-        self.board.move(0, 0)
-        self.assertTrue(self.board.player_has_lost())
-        self.board.undo_move()
-        self.board.move(1, 0)
-        self.assertTrue(self.board.player_has_lost())
-        self.board.undo_move()
-        self.board.move(2, 0)
-        self.assertTrue(self.board.player_has_lost())
-        self.board.undo_move()
-        self.board.move(2, 1)
-        self.assertTrue(self.board.player_has_lost())
+def test_str():
+    board = mnk.Board(3, 3, 2)
+    assert str(board) == '_|_|_\n'*3
 
-if __name__ == '__main__':
-    unittest.main()
+def test_move():
+    board = mnk.Board(3, 3, 2)
+    board.move(0, 0)
+    board.move(1, 2)
+    board.move(2, 1)
+    assert str(board) == (
+        '_|O|_\n'
+        '_|_|X\n'
+        'X|_|_\n')
+
+def test_undo_move():
+    board = mnk.Board(3, 3, 2)
+    board.move(0, 0)
+    board.undo_move()
+    assert str(board) == '_|_|_\n'*3
+
+def test_legal_moves():
+    board = mnk.Board(3, 3, 2)
+    assert len(board.legal_moves()) == 9
+    board.move(0, 0)
+    assert len(board.legal_moves()) == 8
+
+def test_player_has_lost():
+    board = mnk.Board(3, 3, 2)
+    board.move(1, 1)
+    board.move(2, 2)
+    assert not board.player_has_lost()
+    board.move(0, 0)
+    assert board.player_has_lost()
+    board.undo_move()
+    board.move(1, 0)
+    assert board.player_has_lost()
+    board.undo_move()
+    board.move(2, 0)
+    assert board.player_has_lost()
+    board.undo_move()
+    board.move(2, 1)
+    assert board.player_has_lost()
